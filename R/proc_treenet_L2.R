38478cf2106761fa7c79e722e6ad39da5441fbea
Simon Knüsel <simonknusel@MacBook-Pro-4.wsl.ch>
2019-03-19 14:35:13 +0100

    new documentation

---
+++ R/proc_treenet_L2.R
@@ -0,0 +1,78 @@
+#' Process Treenet Data to L2
+#'
+#' \code{proc_treenet_L2()} processes L0 dendrometer data from the treenet
+#'   server directly to L2.
+#'
+#' @param plot specify whether a comparison of \code{L1} and \code{L2} data
+#'   should be plotted.
+#' @inheritParams select_data
+#' @inheritParams proc_L1
+#' @inheritParams proc_dendro_L2
+#' @inheritParams plot_dendro
+#'
+#' @return The function returns:
+#'  a \code{data.frame} with processed dendrometer data containing the
+#'  following columns
+#'    \item{series}{name of the series.}
+#'    \item{ts}{timestamp with format \code{\%Y-\%m-\%d \%H:\%M:\%S}.}
+#'    \item{value}{dendrometer value.}
+#'    \item{max}{highest measured value up to this timestamp.}
+#'    \item{twd}{tree water deficit, i.e. the amount of stem shrinkage
+#'      expressed as the difference between \code{max} and \code{value}.}
+#'    \item{mds}{maximum daily shrinkage, calculated as the difference between
+#'      a local maximum that occurs before a local minimum during one day. If
+#'      there is no local maximum or minimum or if the minimum occurs prior to
+#'      the maximum, then \code{mds = NA}. This may occur on days with rain or
+#'      in winter.}
+#'    \item{gro_yr}{growth since the beginning of the year. Also calculated if
+#'      for years with missing data.}
+#'    \item{flags}{number specifying whether and which changes occurred during
+#'      the processing.}
+#'    \item{version}{processing version.}
+#'
+#' @seealso \code{\link{proc_L1}} to process dendrometer or climate data to
+#'   \code{L1} only (i.e. time-aligned data), \code{\link{proc_dendro_L2}}
+#'   to process dendrometer data from \code{L1} to \code{L2}.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' proc_treenet_L2(site = "sagno", year = "full", period = "yearly")
+#'
+#' proc_treenet_L2(site = "sagno", year = "obx")
+#' }
+proc_treenet_L2 <- function(site = NULL, sensor_name = NULL,
+                            from = NULL, to = NULL, path_cred = NULL,
+                            temp_name = NULL, reso = 10, year = "asis",
+                            val_range = c(0, 20000), diffwin = 2000,
+                            diffsum = 1000, tz = "Etc/GMT-1", plot = TRUE,
+                            period = "full", add = TRUE) {
+
+  # Check input variables -----------------------------------------------------
+  if (plot != TRUE & plot != FALSE) {
+    stop("provide 'plot' with either TRUE or FALSE")
+  }
+
+
+  # Process data to L2 --------------------------------------------------------
+  print("download data from server...")
+  df_L0 <- select_data(site = site, sensor_name = sensor_name,
+                       from = from, to = to, path_cred = path_cred,
+                       temp_name = temp_name)
+
+  print("process data to L1 (time-aligned data)...")
+  df_L1 <- proc_L1(data = df_L0, reso = reso, year = year, tz = tz)
+
+  print("process data to L2...")
+  df_L2 <- proc_dendro_L2(dendro_data = df_L1, val_range = val_range,
+                          diffwin = diffwin, diffsum = diffsum, tz = tz)
+
+  print("plot data...")
+  if (plot) {
+    plot_dendro(data_L1 = df_L1, data_L2 = df_L2, period = period, add = add,
+                tz = tz)
+  }
+  print("Done!")
+  return(df_L2)
+}
---
+++ R/select_data.R
@@ -0,0 +1,228 @@
+#' Select Dendrometer and Temperature Data from Treenet Server
+#'
+#' \code{select_data()} accesses and downloads dendrometer and temperature
+#'   data from treenet server. Function imported and adapted from package
+#'   \code{treenetdown}.
+#'
+#' @param site Character string specifying the site. String is not
+#'   case sensitive.
+#' @param sensor_name Character string specifying the name of a single or
+#'   multiple sensors (e.g. \code{"LWF-Demo-1.dendrometer.ch0"}).
+#' @param from Optional argument to select data after a specific date
+#'   (\code{"YYYY-MM-DD"}).
+#' @param to Optional argument to select data up to a specific date
+#'   (\code{"YYYY-MM-DD"}).
+#' @param path_cred Optional argument to specify the full path to the
+#'   file \code{config.yml} containing the database login data. File can
+#'   also be copied to the \code{Data} folder of the package for automatic
+#'   recognition.
+#' @param temp_name Optional argument to specify the series name of a
+#'   temperature dataset. Needed if there are no or multiple on-site air
+#'   temperature measurements. If \code{temp_name} is not specified and no
+#'   temperature dataset is found on the server, a sample dataset will be
+#'   used with a warning.
+#' @inheritParams proc_L1
+#' @inheritParams proc_dendro_L2
+#'
+#' @keywords internal
+#'
+#' @examples
+#' \dontrun{
+#' select_data(site = "birmensdo")
+#' }
+#'
+select_data <- function(site = NULL, sensor_name = NULL,
+                        from = NULL, to = NULL, path_cred = NULL,
+                        temp_name = NULL) {
+
+  # Check input variables -----------------------------------------------------
+  if (length(site) == 0 & length(sensor_name) == 0) {
+    stop(paste0("Specify at least one of the following: site, ",
+                "region, sensor_name."))
+  }
+  if (length(site) > 1) {
+    stop("You can only specify one site at a time.")
+  }
+  if (length(from) != 0) {
+    if (is.na(as.Date(from, format = "%Y-%m-%d"))) {
+      stop("Provide 'from' in date format. Format needs to be 'YYYY-MM-DD'.")
+    }
+  }
+  if (length(to) != 0) {
+    if (is.na(as.Date(to, format = "%Y-%m-%d"))) {
+      stop("Provide 'to' in date format. Format needs to be 'YYYY-MM-DD'.")
+    }
+  }
+  if (length(temp_name) > 1) {
+    stop("Provide single temperature dataset with 'temp_name'.")
+  }
+
+
+  # Database credentials ------------------------------------------------------
+  if (length(path_cred) == 0) {
+    if (file.exists(file.path(system.file("extdata", "config.yml",
+                                          package = "treenetproc")))) {
+      path_cred <- file.path(system.file("extdata", "config.yml",
+                                         package = "treenetproc"))
+    } else {
+      stop("File with database credentials not found. Please move the ",
+           "file 'config.yml' to the folder of the package or specify ",
+           "an alternative path in the function call with 'path_cred = ...'")
+    }
+  }
+
+
+  # Load functions ------------------------------------------------------------
+  setUTC1 <- function() {
+    temp <- as.character(sqldf::sqldf("show timezone", connection = con))
+    if (temp != "Etc/GMT-1") {
+      xx <- sqldf::sqldf("SET TIME ZONE 'Etc/GMT-1'", connection = con)
+    }
+    temp <- as.character(sqldf::sqldf("show timezone", connection = con))
+    if (temp != "Etc/GMT-1") {
+      stop("Error with timestamp in database.")
+    }
+  }
+
+
+  # Specify series for download via metadata file -----------------------------
+  Sys.setenv(TZ = "Etc/GMT-1")
+  options(warn = -1)
+
+  spread <- suppressMessages(googlesheets::gs_title("Metadata"))
+  meta <- suppressMessages(as.data.frame(
+    googlesheets::gs_read(ss = spread, ws = "Metadata")))
+
+  meta_filter <- meta$Seriesname
+  # site
+  if (!is.null(site)) {
+    meta_select <- vector()
+    for (t in 1:length(site)) {
+      meta_sub <- meta %>%
+        dplyr::filter(Seriesname %in% meta_filter) %>%
+        dplyr::filter(grepl(paste0(site[t]), Site, ignore.case = TRUE))
+
+      if (nrow(meta_sub) != 0) {
+        meta_select <- c(meta_select, meta_sub$Seriesname)
+      } else {
+        stop(paste0("Site '", site[t],"' does not exist."))
+      }
+    }
+    meta_filter <- meta_select
+  }
+  # select temperature data
+  sample_temp <- FALSE
+  if (length(temp_name) == 0) {
+    meta_airtemp <- vector()
+    meta_sub <- meta %>%
+      dplyr::filter(Seriesname %in% meta_filter) %>%
+      dplyr::filter(grepl("airtemperature", Sensor_query, ignore.case = TRUE))
+
+    if (nrow(meta_sub) == 1) {
+      meta_airtemp <- meta_sub$Seriesname
+    }
+    if (nrow(meta_sub) == 0) {
+      message("No air temperature data found. Sample temperature dataset will",
+              "be used.")
+      sample_temp <- TRUE
+    }
+    if (nrow(meta_sub) > 1) {
+      stop("Multiple air temperature datasets found. Please specify the ",
+           "temperature sensor name explicitly in 'temp_name'.")
+    }
+    } else {
+      meta_airtemp <- vector()
+      meta_sub <- meta %>%
+        dplyr::filter(grepl(temp_name, Sensor_query, ignore.case = TRUE))
+
+      if (nrow(meta_sub) == 1) {
+        meta_airtemp <- meta_sub$Seriesname
+      }
+      if (nrow(meta_sub) == 0) {
+        stop(paste0("No air temperature data with 'temp_name = ",
+                    temp_name, " found. Provide a valid 'temp_name'."))
+      }
+    }
+  # sensor_name
+  if (!is.null(sensor_name)) {
+    meta_select <- vector()
+    for (t in 1:length(sensor_name)) {
+      meta_sub <- meta %>%
+        dplyr::filter(Seriesname %in% meta_filter) %>%
+        dplyr::filter(grepl(paste0(sensor_name[t]), Seriesname,
+                            ignore.case = TRUE))
+
+      if (nrow(meta_sub) != 0) {
+        meta_select <- c(meta_select, meta_sub$Seriesname)
+      } else {
+        stop(paste0("Sensor name '", sensor_name[t], "' does not exist."))
+      }
+    }
+    meta_filter <- meta_select
+  }
+  # select dendrometers
+  meta_select <- vector()
+  meta_sub <- meta %>%
+    dplyr::filter(Seriesname %in% meta_filter) %>%
+    dplyr::filter(grepl("dendrometer", Sensor_query,
+                        ignore.case = TRUE))
+
+  if (nrow(meta_sub) != 0) {
+    meta_select <- c(meta_select, meta_sub$Seriesname)
+    } else {
+      stop("No dendrometer data found.")
+    }
+  meta_filter <- meta_select
+  if (length(meta_airtemp) != 0) {
+    meta_filter <- c(meta_filter, meta_airtemp)
+  }
+
+  # format (always L0)
+  db_folder <- "treenet0"
+  db_version <- "version = 0;"
+  select_col <- c("series", "ts", "value")
+  version_nr <- 0
+
+
+  # Download series -----------------------------------------------------------
+  treenet_data <- list()
+  for (i in 1:length(meta_filter)) {
+    drv <- RPostgres::Postgres()
+    cred <- config::get("treenet_cred", file = path_cred)
+    con <- DBI::dbConnect(drv,
+                          dbname = cred$dbname,
+                          host = cred$host,
+                          port = cred$port,
+                          user = cred$user,
+                          password = cred$password)
+    setUTC1()
+
+    foo <- sqldf::sqldf(paste0("SELECT * from ", db_folder,
+                               " where series = '", meta_filter[i], "' AND ",
+                               db_version), connection = con)
+    invisible(DBI::dbDisconnect(con))
+
+    df <- foo %>%
+      dplyr::select(match(select_col, names(.))) %>%
+      transform(ts = as.POSIXct(ts, format = "%m-%d-%y %H:%M:%S",
+                                tz = "Etc/GMT-1")) %>%
+      dplyr::arrange(ts) %>%
+      dplyr::distinct() %>%
+      dplyr::filter(ts <= Sys.time()) %>%
+      transform(value = as.numeric(value))
+
+    treenet_data[[i]] <- df
+  }
+
+  options(warn = 0)
+  df <- dplyr::bind_rows(treenet_data) %>%
+    dplyr::mutate(version = version_nr) %>%
+    dplyr::select(series, ts, value, version)
+
+  if (sample_temp) {
+    tem <- create_temp_dummy_treenet(df = df)
+    df <- dplyr::bind_rows(df, tem)
+  }
+
+  return(df)
+}
