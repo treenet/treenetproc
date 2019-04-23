21c8b3020f409e352a1306c332d39f6f4d3bafbf
Simon Knüsel <simonknusel@MacBook-Pro-4.wsl.ch>
2019-04-23 14:38:46 +0200

    differences are numbered and removed differences are plotted in grey.

--- R/plot_proc_L2.R
+++ R/plot_proc_L2.R
@@ -2,24 +2,19 @@
 #'
 #' \code{plot_proc_L2} provides plots of time-aligned (\code{L1}) and
 #'   processed (\code{L2}) dendrometer data to visually assess the processing.
-#'   The first panel shows the \code{L1} data and the second panel the
-#'   processed \code{L2} data. The third panel shows the daily difference
-#'   between \code{L1} and \code{L2} data. Large differences without an
-#'   apparent jump in the data indicate problems in the processing.
 #'
 #' @param data_L1 time-aligned dendrometer data as produced by
 #'   \code{\link{proc_L1}}.
 #' @param data_L2 processed dendrometer data as produced by
 #'   \code{\link{proc_dendro_L2}}.
-#' @param period specify whether plots should be displayed over the whole
-#'   period (\code{period = "full"}), for each year separately
-#'   (\code{period = "yearly"}) or for each month (\code{period = "monthly"}).
-#' @param show specify whether all periods should be plotted
-#'   (\code{show = "all"}) or only those periods in which \code{L1} and
+#' @param plot_period specify whether plots should be displayed over the whole
+#'   plot_period (\code{plot_period = "full"}), for each year separately
+#'   (\code{plot_period = "yearly"}) or for each month
+#'   (\code{plot_period = "monthly"}).
+#' @param plot_show specify whether all periods should be plotted
+#'   (\code{plot_show = "all"}) or only those periods in which \code{L1} and
 #'   \code{L2} dendrometer data differ after processing
-#'   (\code{show = "diff"}).
-#' @param add logical, specify whether \code{L1} data should be plotted along
-#'   with \code{L2} data in the second panel of the plot.
+#'   (\code{plot_show = "diff"}).
 #' @param plot_name character, specify name of the PDF in which the plots are
 #'   saved.
 #' @inheritParams proc_L1
@@ -27,38 +22,53 @@
 #' @return Plots are saved to current working directory as
 #'   \code{proc_L2_plot.pdf} or as specified in \code{plot_name}.
 #'
+#'   The first panel shows the \code{L1} data, the second panel the
+#'   processed \code{L2} data with the \code{L1} data in the background. The
+#'   third panel shows the absolute value of the differences between \code{L1}
+#'   and \code{L2} data. Large differences without an apparent jump in the
+#'   data indicate problems in the processing.
+#'
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' plot_proc_L2(data_L1 = data_L1_dendro, data_L2 = data_L2_dendro,
-#'             period = "yearly")
+#'             plot_period = "yearly")
 #' }
-plot_proc_L2 <- function(data_L1, data_L2, period = "full", show = "all",
-                         tz = "Etc/GMT-1", add = TRUE,
+plot_proc_L2 <- function(data_L1, data_L2, plot_period = "full",
+                         plot_show = "all", tz = "Etc/GMT-1",
                          plot_name = "proc_L2_plot") {
 
   # Check input variables -----------------------------------------------------
-  if (!(period %in% c("full", "yearly", "monthly"))) {
-    stop("period needs to be either 'full', 'yearly' or 'monthly'.")
+  if (!(plot_period %in% c("full", "yearly", "monthly"))) {
+    stop("plot_period needs to be either 'full', 'yearly' or 'monthly'.")
   }
-  if (!(show %in% c("all", "diff"))) {
-    stop("show needs to be either 'all' or 'diff'.")
+  if (!(plot_show %in% c("all", "diff"))) {
+    stop("plot_show needs to be either 'all' or 'diff'.")
   }
-  check_logical(var = add, var_name = "add")
   check_data_L1(data_L1 = data_L1)
   check_data_L2(data_L2 = data_L2)
 
 
-  # Calculate daily difference ------------------------------------------------
+  # Calculate differences -----------------------------------------------------
+  # add diff_plot_old, used to plot removed differences
+  if (!("diff_plot_old" %in% colnames(data_L1))) {
+    data_L1$diff_plot_old <- NA
+  }
   data_L1 <- data_L1 %>%
     dplyr::mutate(year = strftime(ts, format = "%Y", tz = tz)) %>%
     dplyr::mutate(month = strftime(ts, format = "%m", tz = tz)) %>%
-    dplyr::mutate(day = strftime(ts, format = "%d", tz = tz))
+    dplyr::group_by(series) %>%
+    dplyr::mutate(diff_L1 = c(NA, diff(value, lag = 1))) %>%
+    dplyr::mutate(value_L1 = value) %>%
+    dplyr::ungroup()
   data_L2 <- data_L2 %>%
     dplyr::mutate(year = strftime(ts, format = "%Y", tz = tz)) %>%
     dplyr::mutate(month = strftime(ts, format = "%m", tz = tz)) %>%
-    dplyr::mutate(day = strftime(ts, format = "%d", tz = tz))
+    dplyr::group_by(series) %>%
+    dplyr::mutate(diff_L2 = c(NA, diff(value, lag = 1))) %>%
+    dplyr::mutate(value_L2 = value) %>%
+    dplyr::ungroup()
 
   sensors <- unique(data_L1$series)
   years <- unique(data_L1$year)
@@ -73,26 +83,22 @@ plot_proc_L2 <- function(data_L1, data_L2, period = "full", show = "all",
       dplyr::filter(series == sensor_label)
 
     diff_sensor <- data_L1_sensor %>%
-      dplyr::select(series, ts, value_L1 = value) %>%
+      dplyr::select(series, ts, value_L1, diff_L1, diff_plot_old) %>%
       dplyr::full_join(., data_L2_sensor, by = c("series", "ts")) %>%
-      dplyr::select(series, ts, value_L1, value_L2 = value, year,
-                    month, day) %>%
-      dplyr::group_by(year, month, day) %>%
-      dplyr::mutate(
-        value_L1_zero = value_L1 - value_L1[which(!is.na(value_L1))[1]]) %>%
-      dplyr::mutate(
-        value_L2_zero = value_L2 - value_L2[which(!is.na(value_L2))[1]]) %>%
-      dplyr::summarise(
-        diff =
-          abs(dplyr::last(value_L1_zero[which(!is.na(value_L1_zero))]) -
-                dplyr::last(value_L2_zero[which(!is.na(value_L2_zero))]))) %>%
-      dplyr::ungroup() %>%
-      dplyr::mutate(ts = as.POSIXct(paste0(year, "-", month, "-", day),
-                                    format = "%Y-%m-%d", tz = tz))
+      dplyr::select(series, ts, value_L1, value_L2, diff_L1, diff_L2,
+                    diff_plot_old, year, month) %>%
+      dplyr::mutate(diff = diff_L1 - diff_L2) %>%
+      dplyr::mutate(diff = ifelse(abs(diff) <= 0.001, 0, diff)) %>%
+      dplyr::mutate(diff = ifelse(is.na(diff), 0, diff)) %>%
+      dplyr::mutate(diff_plot = abs(diff)) %>%
+      dplyr::mutate(diff_nr = 0) %>%
+      dplyr::mutate(diff_nr = ifelse(diff != 0, 1, 0)) %>%
+      dplyr::mutate(diff_nr = cumsum(diff_nr)) %>%
+      dplyr::mutate(diff_nr = ifelse(diff == 0, NA, diff_nr))
 
 
     # Plot L1, L2 and daily diff ----------------------------------------------
-    if (period %in% c("yearly", "monthly")) {
+    if (plot_period %in% c("yearly", "monthly")) {
 
       for (y in 1:length(years)) {
         year_label <- years[y]
@@ -108,22 +114,22 @@ plot_proc_L2 <- function(data_L1, data_L2, period = "full", show = "all",
         diff_year <- diff_sensor %>%
           dplyr::filter(year == year_label)
 
-        if (period == "yearly") {
+        if (plot_period == "yearly") {
           if (sum(!is.na(data_L1_year$value)) != 0 &
               sum(!is.na(data_L2_year$value)) != 0) {
-            if (show == "diff" &
+            if (plot_show == "diff" &
                 max(abs(diff_year$diff), na.rm = TRUE) < 0.1) {
               next
             }
             plotting_proc_L2(data_L1 = data_L1_year, data_L2 = data_L2_year,
-                             diff = diff_year, period = period, add = add,
+                             diff = diff_year, plot_period = plot_period,
                              tz = tz)
           } else {
             next
           }
         }
 
-        if (period == "monthly") {
+        if (plot_period == "monthly") {
           months <- unique(data_L1_year$month)
 
           for (m in 1:length(months)) {
@@ -141,15 +147,38 @@ plot_proc_L2 <- function(data_L1, data_L2, period = "full", show = "all",
             diff_month <- diff_year %>%
               dplyr::filter(month == month_label)
 
+            if (sum(!is.na(diff_month$diff_nr)) != 0 |
+                sum(!is.na(diff_month$diff_plot_old))) {
+
+              diff_month_label <- diff_month %>%
+                dplyr::mutate(day = strftime(ts, format = "%d", tz = tz)) %>%
+                dplyr::filter(!is.na(diff_nr) | !is.na(diff_plot_old)) %>%
+                dplyr::group_by(year, month, day) %>%
+                dplyr::mutate(diff_nr_first = dplyr::first(diff_nr),
+                              diff_nr_last = dplyr::last(diff_nr)) %>%
+                dplyr::mutate(
+                  diff_nr = ifelse(diff_nr_first != diff_nr_last,
+                                   paste0(diff_nr_first, "-", diff_nr_last),
+                                   as.character(diff_nr_first))) %>%
+                dplyr::summarise(ts = ts[1],
+                                 diff_nr = diff_nr[1]) %>%
+                dplyr::ungroup() %>%
+                dplyr::select(ts, diff_nr)
+
+              diff_month <- diff_month %>%
+                dplyr::select(-diff_nr) %>%
+                dplyr::left_join(., diff_month_label, by = "ts")
+            }
+
             if (sum(!is.na(data_L1_month$value)) != 0 &
                 sum(!is.na(data_L2_month$value)) != 0) {
-              if (show == "diff" &
+              if (plot_show == "diff" &
                   max(abs(diff_month$diff), na.rm = TRUE) < 0.1) {
                 next
               }
               plotting_proc_L2(data_L1 = data_L1_month,
                                data_L2 = data_L2_month,
-                               diff = diff_month, period = period, add = add,
+                               diff = diff_month, plot_period = plot_period,
                                tz = tz)
             } else {
               next
@@ -159,14 +188,14 @@ plot_proc_L2 <- function(data_L1, data_L2, period = "full", show = "all",
       }
     }
 
-    if (period == "full") {
+    if (plot_period == "full") {
       year_label <- paste0(years[1], "-", years[length(years)])
       passenv$year_label <- year_label
 
       if (sum(!is.na(data_L1_sensor$value)) != 0 &
           sum(!is.na(data_L2_sensor$value)) != 0) {
         plotting_proc_L2(data_L1 = data_L1_sensor, data_L2 = data_L2_sensor,
-                         diff = diff_sensor, period = period, add = add,
+                         diff = diff_sensor, plot_period = plot_period,
                          tz = tz)
       } else {
         next
