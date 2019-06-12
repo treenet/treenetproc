610ed74f0ff604d1795e8551242587f5db29a75a
Simon Knüsel <simonknusel@MacBook-Pro-4.wsl.ch>
2019-06-12 19:09:08 +0200

    adapted description.

--- R/proc_L1.r
+++ R/proc_L1.r
@@ -1,21 +1,12 @@
 #' Process Raw Dendrometer or Meteorological Data to L1
 #'
-#' \code{proc_L1()} processes raw dendrometer and raw meteorological data to
+#' \code{proc_L1} processes raw dendrometer and raw meteorological data to
 #'   time-aligned data.
 #'
 #' @param data input \code{data.frame} containing raw dendrometer or
-#'   meteorological data. The input data needs to contain a timestamp column
-#'   (named \code{ts}). If \code{ts} is not provided in the standard format
-#'   (\code{\%Y-\%m-\%d \%H:\%M:\%S}) the format needs to be specified in
-#'   \code{format_date}. Data of multiple sensors can either be in \code{long}
-#'   format \code{input = "long"} with a column named \code{series} to
-#'   differentiate the sensors or in \code{wide} format \code{input = "wide"}
-#'   with sensors in separate columns. The name of temperature and
-#'   precipitation data have to at least contain \code{temp} or \code{prec},
-#'   respectively.
-#' @param reso desired time resolution of output (in minutes). Resolution
-#'   should be chosen to be close to the actual measurement intervals.
-#'   \code{reso} needs to be a multiple of 5.
+#'   meteorological data (see Details for formatting requirements).
+#' @param reso desired time resolution of output (in minutes). See
+#'   \code{Details} for more information on data aggregation.
 #' @param year if \code{year = "full"} then the output \code{data.frame}
 #'   contains data of complete years, i.e. \code{YYYY-01-01 to YYYY-12-31}; if
 #'   \code{year = "asis"} the output \code{data.frame} covers the same period
@@ -25,13 +16,23 @@
 #'   \code{series}) or in \code{"wide"} format (i.e. one sensor per column).
 #' @param date_format specify a custom date format if it is not
 #'   \code{\%Y-\%m-\%d \%H:\%M:\%S}.
-#' @param tz specify the desired time zone. Default is \code{"Etc/GMT-1"}.
+#' @param tz specify the desired time zone. Default is \code{"UTC"}.
 #'
-#' @details Data at \emph{irregular} time steps: Data is linearly interpolated
-#'   between the irregular time steps closest to the regular time step.
+#' @details The input data needs to contain a timestamp column
+#'   (named \code{ts}). If \code{ts} is not provided in the standard format
+#'   (\code{\%Y-\%m-\%d \%H:\%M:\%S}) the format needs to be specified in
+#'   \code{format_date}.\cr
+#'   Data of multiple sensors can either be in \code{long}
+#'   format (\code{input = "long"}) with a column named \code{series} to
+#'   differentiate the sensors or in \code{wide} format (\code{input = "wide"})
+#'   with sensors in separate columns.\cr
+#'   Temperature and precipitation data must contain \code{temp} or
+#'   \code{prec}, respectively, somewhere in their series name.
 #'
-#'   Data at \emph{regular} time steps: Values corresponding to regular time
-#'   stamps are selected.
+#'   To align data at regular time intervals defined in \code{reso}, the data
+#'   is linearly interpolated between the two values measured closest to the
+#'   regular time intervals. If data is already provided at regular time steps,
+#'   the corresponding values are selected.
 #'
 #' @return a \code{data.frame} with measurements aligned to regular time
 #'   intervals (interval specified in \code{reso}) containing the following
@@ -44,11 +45,11 @@
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' proc_L1(data = data_L0_long)
-#' }
+#' proc_L1(data = dendro_data_L0)
+#' proc_L1(data = dendro_data_L0_wide, input = "wide")
+#'
 proc_L1 <- function(data, reso = 10, year = "asis", input = "long",
-                    date_format = "%Y-%m-%d %H:%M:%S", tz = "Etc/GMT-1") {
+                    date_format = "%Y-%m-%d %H:%M:%S", tz = "UTC") {
 
   # Check input variables -----------------------------------------------------
   if (!(year %in% c("asis", "full"))) {
@@ -61,12 +62,13 @@ proc_L1 <- function(data, reso = 10, year = "asis", input = "long",
 
   # Check input data ----------------------------------------------------------
   df <- data
-  check_format(df = df, input = input)
   df <- check_ts(df = df, date_format = date_format, tz = tz)
+  check_format(df = df, input = input)
 
 
   # Format input data ---------------------------------------------------------
   df <- format_input(df = df, input = input, tz = tz)
+  df <- check_missing(df = df)
   series_vec <- unique(df$series)
 
 
