024ee5207b3fa4779ed4fc270670783def28b23c
Simon Knüsel <simonknusel@MacBook-Pro-4.wsl.ch>
2019-06-12 18:59:49 +0200

    subsetting to specified series is now done first.

--- R/corr_dendro_L1.R
+++ R/corr_dendro_L1.R
@@ -5,28 +5,28 @@
 #'   overwritten with \code{NA} without further notice.
 #'
 #' @param plot logical, plots the corrected dataset with the original data
-#'   in the back.
+#'   in the background in grey.
 #' @inheritParams plot_proc_L2
 #' @inheritParams corr_dendro_L3
 #'
-#' @return
+#' @return The function returns a \code{data.frame} with corrected \code{L1}
+#'   dendrometer data.
+#'
+#' @seealso \code{\link{corr_dendro_L3}} to correct \code{L2} data.
 #'
 #' @export
 #'
 #' @examples
+#' corr_dendro_L1(data_L1 = dendro_data_L1,
+#'                delete = c("2016-08-01", "2016-08-05"),
+#'                series = "site-1_dendro-3", plot_export = FALSE)
 #'
 corr_dendro_L1 <- function(data_L1, delete, series = NULL, plot = TRUE,
-                           tz = "Etc/GMT-1") {
+                           plot_export = TRUE, tz = "UTC") {
 
-  # Check input variables -----------------------------------------------------
-  check_data_L1(data_L1 = data_L1)
-  delete <- check_datevec(datevec = delete, tz = tz)
-  check_delete(delete = delete)
+  # Subset data to selected series --------------------------------------------
   check_series(df = data_L1, series = series)
-  check_logical(var = plot, var_name = "plot")
 
-
-  # Remove dendrometer data in specified periods ------------------------------
   series_select <- series
   n_series <- length(unique(data_L1$series))
 
@@ -44,8 +44,26 @@ corr_dendro_L1 <- function(data_L1, delete, series = NULL, plot = TRUE,
       dplyr::filter(series != series_select)
   }
 
+
+  # Check input variables -----------------------------------------------------
+  check_data_L1(data_L1 = data_L1)
+  delete <- check_datevec(datevec = delete, tz = tz)
+  check_delete(delete = delete)
+  check_date_period(datevec = delete, datevec_name = "delete", df = df)
+  check_logical(var = plot, var_name = "plot")
+
+
+  # Remove dendrometer data in specified periods ------------------------------
+  # remove leading and trailing NA's
+  na_list <- remove_lead_trail_na(df = df)
+  df <- na_list[[1]]
+  lead_trail_na <- na_list[[2]]
+
   df <- deleteperiod(df = df, delete = delete) %>%
     dplyr::select(-flagdelete)
+
+  # append leading and trailing NA's
+  df <- append_lead_trail_na(df = df, na = lead_trail_na)
 
 
   # Plot changes --------------------------------------------------------------
@@ -61,8 +79,9 @@ corr_dendro_L1 <- function(data_L1, delete, series = NULL, plot = TRUE,
       dplyr::mutate(month = paste0(substr(ts, 1, 7), "-01")) %>%
       dplyr::filter(month %in% month_plot)
 
-    plot_L1(data_L1 = df_plot, plot_period = "monthly", tz = tz,
-            data_L1_orig = data_L1_orig, plot_name = "corr_L1_plot")
+    plot_L1(data_L1 = df_plot, data_L1_orig = data_L1_orig,
+            plot_period = "monthly", plot_export = plot_export,
+            plot_name = "corr_L1_plot", tz = tz)
   }
 
   if (n_series > 1) {
