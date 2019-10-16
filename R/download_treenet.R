83fab49b3c0d4cae8c3bfc727291f9bd982dc770
simknu <simon.knuesel@wsl.ch>
2019-10-16 13:28:01 +0200

    - series are downloaded with functions select_series and download_series - after downloading L1 and L2 data are again time-aligned (since periods of NA are not stored on the server)

--- R/download_treenet.R
+++ R/download_treenet.R
@@ -3,7 +3,12 @@
 #' \code{download_treenet} loads \code{L0, L1 or L2} dendrometer data from
 #'   the TreeNet or Decentlab server.
 #'
-#' @inheritParams select_data
+#' @param export logical, indicate whether each sensor will be saved as
+#'   an \code{.RData}-file in the current working directory
+#'   (\code{export = TRUE}) or will be returned to the console.
+#' @inheritParams select_series
+#' @inheritParams download_series
+#' @inheritParams proc_L1
 #'
 #' @return If \code{export = TRUE} data will be saved in a named list with
 #'   each list element containing one sensor.
@@ -21,11 +26,10 @@
 #'                  server = "decentlab")
 #' }
 download_treenet <- function(site = NULL, sensor_name = NULL,
-                             from = NULL, to = NULL,
-                             server = "treenet",
-                             data_format = "L0", path_cred = NULL,
-                             export = FALSE, bind_df = TRUE,
-                             last = NULL, tz = "Etc/GMT-1") {
+                             sensor_class = NULL, from = NULL, to = NULL,
+                             server = "treenet", data_format = "L0",
+                             path_cred = NULL, export = FALSE, last = NULL,
+                             temp_ref = FALSE, tz = "Etc/GMT-1") {
 
   # Check input variables -----------------------------------------------------
   list_inputs <- mget(ls())
@@ -34,11 +38,48 @@ download_treenet <- function(site = NULL, sensor_name = NULL,
 
   # Download data from server -------------------------------------------------
   print("download data from server...")
-  df_server <- select_data(site = site, sensor_name = sensor_name,
-                           from = from, to = to, server = server,
-                           data_format = data_format, path_cred = path_cred,
-                           select_temp = FALSE, export = export,
-                           bind_df = bind_df, last = last, tz = tz)
+
+  # load credentials
+  path_cred <- load_credentials(path_cred = path_cred)
+
+  # select series and reference temperature for download
+  meta_series <- select_series(site = site, sensor_class = sensor_class,
+                               sensor_name = sensor_name,
+                               path_cred = path_cred)
+
+  # download selected series
+  df_server <- download_series(meta_series = meta_series,
+                               data_format = data_format, from = from,
+                               to = to, last = last, bind_df = TRUE,
+                               reso = reso, path_cred = path_cred,
+                               server = server, temp_ref = temp_ref, tz = tz)
+
+
+  # Time-align downloaded data ------------------------------------------------
+  if (data_format %in% c("L1", "L2")) {
+    # needed for fillintergaps inside tsalign
+    passenv$reso <- 10
+    options(warn = -1)
+    df_server <- tsalign(df = df_server, reso = 10, year = "asis",
+                         tz = tz) %>%
+      dplyr::mutate(series = fill_na(series))
+    options(warn = 0)
+  }
+
+
+  # Return dataset ------------------------------------------------------------
+  # export each series as a .RData file
+  if (export) {
+    print("export data...")
+    series_vec <- unique(df_server$series)
+    for (s in 1:length(series_vec)) {
+      df_single <- df_server %>%
+        dplyr::filter(series == series_vec[s])
+      save(df_single, file = paste0(series_vec[s], "_", data_format,
+                                    ".RData"), compress = "xz")
+    }
+    print("Done!")
+  }
 
   print("Done!")
   return(df_server)
