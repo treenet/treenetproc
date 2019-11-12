f177707c3b9433495651db0b30ecc0b5c2a82af3
simknu <simon.knuesel@wsl.ch>
2019-11-12 14:02:37 +0100

    - extended dendrometer dataset to multiple years (for gro_yr plot) - changed timestamps to year 2013 right after download

--- data-raw/dendro_data.R
+++ data-raw/dendro_data.R
@@ -10,8 +10,10 @@ jump_L0 <- download_treenet(sensor_name = "Beatenberg-0.dendrometer.ch1",
                             from = "2014-10-25", to = "2014-11-10",
                             data_format = "L0", server = "decentlab",
                             tz = "UTC") %>%
+  # set start of ts to 2013-05-01
+  mutate(ts = ts - 46828800) %>%
   mutate(value = value - 7000) %>%
-  mutate(value = ifelse(value > 7000, value - 9000, value)) %>%
+  mutate(value = ifelse(value > 7000, value - 7000, value)) %>%
   mutate(series = "site-1_dendro-1")
 
 # prepare outlier data
@@ -19,21 +21,39 @@ outlier_L0 <- download_treenet(sensor_name = "Jussy-1.dendrometer.ch5",
                                from = "2012-07-12", to = "2012-07-29",
                                data_format = "L0", server = "decentlab",
                                tz = "UTC") %>%
+  # set start of ts to 2013-05-17
+  mutate(ts = ts + 26697600) %>%
   mutate(value = value + (last(jump_L0$value) - first(value)) + 5) %>%
   mutate(series = "site-1_dendro-1")
+
+# download multiyear dendrometer data
+dendro_L0 <- download_treenet(sensor_name = "Pfynwald-02-11.dendrometer.ch0",
+                            from = "2016-06-03", to = "2018-12-31",
+                            data_format = "L0", server = "decentlab",
+                            tz = "UTC") %>%
+  # set to year 2013
+  mutate(ts = ts - 3 * 31556952) %>%
+  mutate(value = value + (last(outlier_L0$value) - first(value)) + 0.3) %>%
+  mutate(series = "site-1_dendro-1")
+
 
 ### prepare shrink data
 shrink_L0 <- download_treenet(sensor_name = "Jussy-1.dendrometer.ch1",
                               from = "2017-08-01", to = "2017-08-31",
                               data_format = "L0", server = "decentlab",
                               tz = "UTC") %>%
+  # set ts to year 2013
+  mutate(ts = ts - 4 * 31556952) %>%
   mutate(series = "site-1_dendro-2")
+
 
 ### prepare delete data
 delete_L0 <- download_treenet(sensor_name = "Lausanne-1.dendrometer.ch2",
                               from = "2018-03-20", to = "2018-03-23",
                               data_format = "L0", server = "decentlab",
                               tz = "UTC") %>%
+  # set start of ts to 2013-08-01
+  mutate(ts = ts - 146188800) %>%
   mutate(value = value * 0.1) %>%
   mutate(series = "site-1_dendro-3")
 
@@ -42,29 +62,31 @@ shrink_2_L0 <- download_treenet(sensor_name = "Pfynwald-02-11.dendrometer.ch0",
                                 from = "2016-08-20", to = "2016-09-12",
                                 data_format = "L0", server = "decentlab",
                                 tz = "UTC") %>%
+  # set start of ts to 2013-08-04
+  mutate(ts = ts - 96076800) %>%
   mutate(value = value + (last(delete_L0$value) - first(value)) - 5) %>%
   mutate(series = "site-1_dendro-3")
 
-shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-27" &
-                    shrink_2_L0$ts <= "2016-08-28"] <- NA
-shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-28"] <-
-  shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-28"] - 490
-shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 17:10:00"] <-
-  shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 17:10:00"] + 10
-shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 17:20:00"] <-
-  shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 17:20:00"] + 15
-shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 17:30:00"] <-
-  shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 17:30:00"] + 25
-shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 17:40:00"] <-
-  shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 17:40:00"] + 35
-shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 17:50:00"] <-
-  shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 17:50:00"] + 20
-shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 18:00:00"] <-
-  shrink_2_L0$value[shrink_2_L0$ts >= "2016-08-31 18:00:00"] + 10
-shrink_2_L0$value[shrink_2_L0$ts >= "2016-09-02 18:30:00"] <-
-  shrink_2_L0$value[shrink_2_L0$ts >= "2016-09-02 18:30:00"] + 10
-shrink_2_L0$value[shrink_2_L0$ts >= "2016-09-03 18:00:00"] <-
-  shrink_2_L0$value[shrink_2_L0$ts >= "2016-09-03 18:00:00"] + 25
+shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-11" &
+                    shrink_2_L0$ts <= "2013-08-12"] <- NA
+shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-12"] <-
+  shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-12"] - 490
+shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 17:10:00"] <-
+  shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 17:10:00"] + 10
+shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 17:20:00"] <-
+  shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 17:20:00"] + 15
+shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 17:30:00"] <-
+  shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 17:30:00"] + 25
+shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 17:40:00"] <-
+  shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 17:40:00"] + 35
+shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 17:50:00"] <-
+  shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 17:50:00"] + 20
+shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 18:00:00"] <-
+  shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-15 18:00:00"] + 10
+shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-17 18:30:00"] <-
+  shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-17 18:30:00"] + 10
+shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-18 18:00:00"] <-
+  shrink_2_L0$value[shrink_2_L0$ts >= "2013-08-18 18:00:00"] + 25
 
 ### prepare frost data
 frost_L0 <- download_treenet(sensor_name = "Jussy-1.dendrometer.ch1",
@@ -87,38 +109,8 @@ frost_L0 <- frost_L0 %>%
                         value))
 
 ### merge L0 data
-dendro_data_L0 <- bind_rows(list(jump_L0, outlier_L0, shrink_L0, delete_L0,
-                                 shrink_2_L0, frost_L0))
-
-# create continous timestamp column for site-1_dendro-1 and site-1_dendro-3
-ts_dendro_1 <- download_treenet(sensor_name = "Beatenberg-0.dendrometer.ch1",
-                                from = "2014-05-01", to = "2015-06-15",
-                                data_format = "L0", server = "decentlab",
-                                tz = "UTC") %>%
-  select(ts) %>%
-  slice(1:length(which(dendro_data_L0$series == "site-1_dendro-1")))
-dendro_data_L0$ts[dendro_data_L0$series == "site-1_dendro-1"] <-
-  ts_dendro_1[, 1]
-
-ts_dendro_3 <-
-  download_treenet(sensor_name = "Pfynwald-02-11.dendrometer.ch0",
-                   from = as.POSIXct("2016-07-31 23:50:00",
-                                     format = "%Y-%m-%d %H:%M:%S",
-                                     tz = "UTC"),
-                   to = "2016-08-31", data_format = "L0",
-                   server = "decentlab") %>%
-  select(ts) %>%
-  slice(1:length(which(dendro_data_L0$series == "site-1_dendro-3")))
-dendro_data_L0$ts[dendro_data_L0$series == "site-1_dendro-3"] <-
-  ts_dendro_3[, 1]
-
-# set the year for all dendrometers to 2013
-dendro_data_L0$ts[dendro_data_L0$series == "site-1_dendro-1"] <-
-  dendro_data_L0$ts[dendro_data_L0$series == "site-1_dendro-1"] - 31520952
-dendro_data_L0$ts[dendro_data_L0$series == "site-1_dendro-2"] <-
-  dendro_data_L0$ts[dendro_data_L0$series == "site-1_dendro-2"] - 4 * 31556952
-dendro_data_L0$ts[dendro_data_L0$series == "site-1_dendro-3"] <-
-  dendro_data_L0$ts[dendro_data_L0$series == "site-1_dendro-3"] - 3 * 31556952
+dendro_data_L0 <- bind_rows(list(jump_L0, outlier_L0, dendro_L0, shrink_L0,
+                                 delete_L0, shrink_2_L0, frost_L0))
 
 # save L0 data
 usethis::use_data(dendro_data_L0, overwrite = TRUE)
@@ -132,7 +124,9 @@ usethis::use_data(dendro_data_L1, overwrite = TRUE)
 
 
 # prepare L2 data -------------------------------------------------------------
-dendro_data_L2 <- proc_dendro_L2(dendro_data = dendro_data_L1, plot = FALSE)
+data("temp_data_L1")
+dendro_data_L2 <- proc_dendro_L2(dendro_data = dendro_data_L1,
+                                 temp_data = temp_data_L1, plot = FALSE)
 
 # save L2 data
 usethis::use_data(dendro_data_L2, overwrite = TRUE)
