9a1ee12e0e6e797537ba4ebdc7fbebe037587602
simknu <simon.knuesel@wsl.ch>
2019-12-04 14:58:24 +0000

    - updated description - added checks for input variables - renamed variable 'tol' to 'tol_seas'

--- R/grow_seas.R
+++ R/grow_seas.R
@@ -1,18 +1,37 @@
-#' Calculate Start and End of Yearly Growth
+#' Calculate Start and End of the Growing Season
 #'
-#' \code{grow_seas} returns the day of the year at which growth starts
-#'   or ends.
+#' \code{grow_seas} returns the day of year at which growth starts
+#'   or ends. Values are returned starting from the second year, since their
+#'   calculation depends on the previous year.
 #'
 #' @param df input \code{data.frame}.
-#' @param tol numeric, defines the amount of yearly growth that needs to be
-#'   surpassed for \code{gro_start} to be defined. \code{1 - tol} is the
+#' @param tol_seas numeric, defines the amount of yearly growth that needs to be
+#'   surpassed for \code{gro_start} to be defined. \code{1 - tol_seas} is the
 #'   amount of yearly growth at which \code{gro_end} is defined.
+#' @param agg_yearly logical, specify whether growth start and end are appended
+#'   to the \code{L2} data or or are exported as a yearly aggregated
+#'   \code{data.frame}.
 #' @inheritParams proc_dendro_L2
 #'
-#' @keywords internal
+#' @export
 #'
-grow_seas <- function(df, tol = 0.05, tz) {
+#' @return
+#'
+#' @examples
+#'
+grow_seas <- function(df, tol_seas = 0.05, agg_yearly = FALSE, tz = "UTC") {
 
+  # Check input variables -----------------------------------------------------
+  list_inputs <- mget(ls())
+  check_input_variables(list = list_inputs)
+
+
+  # Check input data ----------------------------------------------------------
+  check_data_L2(data_L2 = df)
+  reso <- reso_check_L1(df = df)
+
+
+  # Calculate growing season --------------------------------------------------
   if ("gro_start" %in% colnames(df)) {
     df <- df %>%
       dplyr::select(-gro_start)
@@ -22,13 +41,12 @@ grow_seas <- function(df, tol = 0.05, tz) {
       dplyr::select(-gro_end)
   }
 
-  nc <- ncol(df)
   gro_season <- df %>%
     dplyr::mutate(year = strftime(ts, format = "%Y", tz = tz)) %>%
     dplyr::group_by(year) %>%
     dplyr::mutate(gro_tot = sum(gro_yr, na.rm = T)) %>%
-    dplyr::mutate(gro_start_tol = tol * gro_tot) %>%
-    dplyr::mutate(gro_end_tol = (1 - tol) * gro_tot) %>%
+    dplyr::mutate(gro_start_tol = tol_seas * gro_tot) %>%
+    dplyr::mutate(gro_end_tol = (1 - tol_seas) * gro_tot) %>%
     dplyr::mutate(gro_sum = cumsum(ifelse(is.na(gro_yr), 0, gro_yr))) %>%
     dplyr::mutate(
       gro_start_ind = dplyr::first(which(gro_sum >= gro_start_tol))) %>%
