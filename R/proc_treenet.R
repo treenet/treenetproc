19f1fbfb2d84af5ce1b7b7dd24228ba0b1982467
simknu <simon.knuesel@wsl.ch>
2019-12-09 09:42:36 +0000

    updated name of input variable 'df' to 'dendro_L2'

--- R/proc_treenet.R
+++ R/proc_treenet.R
@@ -111,9 +111,9 @@ proc_treenet <- function(site = NULL, sensor_name = NULL,
                             tz = tz)
   }
 
-  df_L2 <- grow_seas(df = df_L2, tz = tz)
-  df_L2 <- phase_stats(df = df_L2, tz = tz, plot_phase = plot_phase,
-                       plot_export = plot_export)
+  df_L2 <- grow_seas(dendro_L2 = df_L2, tz = tz)
+  df_L2 <- phase_stats(dendro_L2 = df_L2, plot_phase = plot_phase,
+                       plot_export = plot_export, tz = tz)
 
   print("Done!")
   return(df_L2)
