dc40daf0b8f0b2f7a107dd1cb15a2f340f9f788c
m wilhelm <micah.wilhelm@wsl.ch>
2021-11-03 11:29:03 +0000

    fix db_time L2M query syntax

--- R/functions_treenet.R
+++ R/functions_treenet.R
@@ -291,13 +291,13 @@ download_series <- function(meta_series, data_format, data_version = NULL,
   db_time <- NULL
   if (length(from) != 0) {
     from    <- as.POSIXct(from, format = "%Y-%m-%d", tz = tz)
-    from.ts <- paste0(" AND ts >= '", format(from, "%Y-%m-%d %H:%M:%S"), "'::timestamp")
-    db_time <- paste(from.ts, to.ts)
+    from.ts <- paste0(" ts >= '", format(from, "%Y-%m-%d %H:%M:%S"), "'::timestamp")
+    db_time <- paste0("WHERE", paste0(c(from.ts, to.ts), collapse = " AND"))
   }
   if (length(to) != 0) {
     to      <- as.POSIXct(to, format = "%Y-%m-%d", tz = tz) + 86399
-    to.ts   <- paste0(" AND ts <= '", format(to, "%Y-%m-%d %H:%M:%S"), "'::timestamp")
-    db_time <- paste(from.ts, to.ts)
+    to.ts   <- paste0(" ts <= '", format(to, "%Y-%m-%d %H:%M:%S"), "'::timestamp")
+    db_time <- paste0("WHERE", paste0(c(from.ts, to.ts), collapse = " AND"))
   }
   if (length(last) != 0) {
     db_time <- paste0(" ORDER BY ts DESC LIMIT ", last)
