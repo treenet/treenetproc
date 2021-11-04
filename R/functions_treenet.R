7c8554c6253ef68fd9b49e72381db8dcd82bab6a
m wilhelm <micah.wilhelm@wsl.ch>
2021-11-04 05:20:36 +0000

    db_time 'AND' query debug

--- R/functions_treenet.R
+++ R/functions_treenet.R
@@ -292,12 +292,12 @@ download_series <- function(meta_series, data_format, data_version = NULL,
   if (length(from) != 0) {
     from    <- as.POSIXct(from, format = "%Y-%m-%d", tz = tz)
     from.ts <- paste0(" ts >= '", format(from, "%Y-%m-%d %H:%M:%S"), "'::timestamp")
-    db_time <- paste0(c("", from.ts, to.ts), collapse = " AND")
+    db_time <- paste0(c(from.ts, to.ts), collapse = " AND")
   }
   if (length(to) != 0) {
     to      <- as.POSIXct(to, format = "%Y-%m-%d", tz = tz) + 86399
     to.ts   <- paste0(" ts <= '", format(to, "%Y-%m-%d %H:%M:%S"), "'::timestamp")
-    db_time <- paste0(c("",from.ts, to.ts), collapse = " AND")
+    db_time <- paste0(c(from.ts, to.ts), collapse = " AND")
   }
   if (length(last) != 0) {
     db_time <- paste0(" ORDER BY ts DESC LIMIT ", last)
@@ -381,7 +381,7 @@ download_series <- function(meta_series, data_format, data_version = NULL,
       } else {
         foo <- sqldf::sqldf(paste0("SELECT * FROM ", db_folder,
                                    " WHERE series = '", series[i], "' AND ",
-                                   db_version, db_time, ";"), connection = con)
+                                   paste0(c(db_version, db_time), collapse = " AND "), ";"), connection = con)
       }
       invisible(DBI::dbDisconnect(con))
     }
